bbf1075d78a12149faa922ff97386e1018b125e3
Philippe Anel <zexigh@gmail.com>
2022-01-21 14:50:23 +0100

    added download and aha1 verification

--- checkenv.py
+++ checkenv.py
@@ -4,13 +4,46 @@ from genericpath import isdir
 import os
 import sys, getopt
 import platform
+import requests
+import hashlib
+
+CEF_WEBSITE="https://cef-builds.spotifycdn.com"
+CEF_WIN64_TARBALL="cef_binary_97.1.6%2Bg8961cdb%2Bchromium-97.0.4692.99_windows64.tar.bz2"
+CEF_WIN64_SHA1="61d5976efb76248ca86b71405f55d84cc9df199b"
+CEF_WIN64_FILE="cef_97.1.6.tar.bz2"
+# CEF_WIN64_TARBALL="cef_binary_97.1.5%2Bg2b00258%2Bchromium-97.0.4692.71_windows64.tar.bz2"
+
+BUF_SIZE=16384
 
 def usage():
-   print('checkenv.py debug|release')
+   print('checkenv.py [debug|release]')
 
 def fatal(error):
    print("[101m" + error + "![0m")
    sys.exit(2)
+
+def compute_sha1(file_name):
+   sha1 = hashlib.sha1()
+   with open(file_name, 'rb') as f:
+      while True:
+         data = f.read(BUF_SIZE)
+         if not data:
+            break
+         sha1.update(data)
+   return "{0}".format(sha1.hexdigest())
+
+def download_file(url, dst):
+   local_filename = url.split('/')[-1]
+   with requests.get(url, stream=True) as r:
+      r.raise_for_status()
+      with open(local_filename, 'wb') as f:
+         sum = 0
+         for chunk in r.iter_content(chunk_size=BUF_SIZE): 
+            sum += 8192 # todo get size
+            print("downloaded %d bytes\r" % sum, end='')
+            f.write(chunk)
+   print("done                                            ")
+   return local_filename   
 
 def main_common(WORKSPACE_STIGMEE = None):
    if WORKSPACE_STIGMEE == None:
@@ -56,6 +89,17 @@ def main(argv):
    main_common(ws)
    if system == "Windows":
       main_windows()
+
+      # download file
+      if os.path.isfile(CEF_WIN64_FILE) == False:
+         download_file(CEF_WEBSITE+"/"+CEF_WIN64_TARBALL, CEF_WIN64_FILE)
+      else:
+         print("%s is already downloaded" % CEF_WIN64_FILE)
+      print("computing %s SHA1" % CEF_WIN64_FILE)
+      hash = compute_sha1(CEF_WIN64_FILE)
+      if hash != CEF_WIN64_SHA1:
+         fatal("invalid hash")
+      print("Hash is ok. Unpacking ...")
 
 if __name__ == "__main__":
    main(sys.argv[1:])
