d34a91ca98f9ea213db9a6b98da9c5d7e30186ab
Quentin Quadrat <lecrapouille@gmail.com>
2022-05-28 18:56:52 +0200

    Try finding a solution to make use Godot inside Docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,11 @@ export WORKSPACE_STIGMEE=$(pwd)
 # ???
 export LIBGL_ALWAYS_INDIRECT=1
 
+Xvfb :99 -ac -screen 0 "1920x1080x24" -nolisten tcp -nolisten unix &
+XVFB_PROC=$!
+sleep 1
+export DISPLAY=:99
+
 # Run as a non root user matching the permission of the actual folder.
 OWNER=$(stat -c '%u' .)
 OWNERGRP=$(stat -c '%g' .)
